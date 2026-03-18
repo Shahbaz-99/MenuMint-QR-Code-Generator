a3150e11ae174384e23aafb8cfa8d85d8176abc3
shahbaz-99 <sahbaj.swn@gmail.com>
2026-03-18 20:57:50 +0530

    added requirements.txt file

--- django_qr/settings.py
+++ django_qr/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m02rjv-3vr80$^tisu93y0689@9b2s$z4e^6e(exz8n^k=56r)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
